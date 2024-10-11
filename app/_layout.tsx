c829418bcc155fae7c4587e4629e84a6cf54ae75
Enzo Aliatis <enzo.aliatis@gmail.com>
2024-10-11 10:52:55 -0500

    Step-6 end

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,28 @@
-import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
